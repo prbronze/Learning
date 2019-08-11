5cc41b2b4c9459cc3d769d063238e1fcce71c88f
prbronze <bronze_santos@hotmail.com>
2019-08-11 11:43:05 -0300

    try/except option to pass stopword(sw) file. if/else in case of sw. stopwords.txt added.

--- NLP/graph2.py
+++ NLP/graph2.py
@@ -4,42 +4,45 @@ import re
 #regex = r"[-'a-zA-ZÀ-ÖØ-öø-ÿ]+"   # raw string
 regex = r"[-'a-zA-ZÀ-ÖØ-öø-ÿ0-9]+"   # raw string
 
-
 if __name__ == '__main__':
     fileName = sys.argv[1]
     weight   = int(sys.argv[2])
 
-    #Leitura de stopwords
-    stopwordsfile = open("stopwords.txt",'r')
-    stopwords     = set([]) 
-    for s in stopwordsfile.readlines():
-        stopwords.add(s.strip().lower())
-
+    #O programa aceita stopwords se quisermos
+    try:
+        stopwordsfile = input('Passe o arquivo das stopwords se houver: ')
+        stopwordsfile = open(stopwordsfile,'r')
+        stopwords     = set([])
+        for s in stopwordsfile.readlines():
+            stopwords.add(s.strip().lower())
+    except IOError:
+        stopwords = 0
+        pass
+    
     document = open(fileName,'r',encoding='utf8')
     content  = document.read()
     
     Words    = re.findall(regex, content)
     Edges    = dict([])
 
-    #Neste implementação o algoritmo ignora a contagem caso alguma das palavras da aresta estiver contida na
-    #lista de stopwords
-    for i in range(0, len(Words)-1):
-        if Words[i] not in stopwords and Words[i+1] not in stopwords:
+    if stopwords != 0:
+        #Neste implementação o algoritmo ignora a contagem caso alguma das
+        #palavras da aresta estiver contida na lista de stopwords
+        for i in range(0, len(Words)-1):
+            if Words[i] not in stopwords and Words[i+1] not in stopwords:
+                edge = (Words[i], Words[i+1])
+                if edge not in Edges:
+                    Edges[edge] = 0
+                Edges[edge] += 1
+    else:
+        for i in range(0, len(Words)-1):
             edge = (Words[i], Words[i+1])
             if edge not in Edges:
                 Edges[edge] = 0
             Edges[edge] += 1
 
-    ####definir função para aceitar *args de stopwords - pensar a lógica
-    '''for i in range(0, len(Words)-1):
-        edge = (Words[i], Words[i+1])
-        if edge not in Edges:
-            Edges[edge] = 0
-        Edges[edge] += 1'''
-
-    ####criar função pra contar com e sem stopwords
+    
     # criando o grafo direcionado (digraph)
-    print(f"Abaixo seguem as 'arestas' de palavras com ocorrência superior a {weight} repetições.")
     txtGraph = "\ndigraph{"
     
     for v in Edges.keys():
@@ -48,6 +51,8 @@ if __name__ == '__main__':
     txtGraph += "\n}"
 
     print(txtGraph)
+    print(f"\nAcima seguem as 'arestas' de palavras com ocorrência superior a {weight} repetições.")
+
     print ( "\nQuantidade de palavras: {}".format(len(Words)) )   
     print ( "Quantidade de arestas : {}".format(len(Edges)) )   
 
