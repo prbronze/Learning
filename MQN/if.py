84d64ea73b4ff3bd587273c1ef957dee901659ee
prbronze <bronze_santos@hotmail.com>
2019-08-14 21:17:16 -0300

    functioning version with mean and stderr plotted

--- MQN/if.py
+++ MQN/if.py
@@ -24,52 +24,49 @@ def integra():
   iList = np.append(iList, np.ones(round((I2-I1)/dt))*Ie )
   iList = np.append(iList, np.zeros(round((tmax-I2)/dt)) )
   freq = 200
-  freqz = 0
+  conta_freq = 0
   
 
-  '''Note que será precisa executar a função
-  diversas vezes para cada Ie e tirar a
-  médias das frequências do neurônio integra-e-dispara.
+  '''Note que será precisa executar a função diversas vezes para cada
+  valor de Ie e tirar a média das frequências do neurônio integra-e-dispara.
  
-  #Abaixo temos uma passgem. Agora precisamos criar
-  um for loop para mais passagens calculando
-  média e std a cada iteração para criar um grafico
-  com barras de stderror/std para cada ponto'''
+  Realizamos 50 loops com a corrente variando entre 1 e 251 nA passos de 10.
+  Armazenamos em t e então calculamos mean e std para cada valor de Ie.
+  Um gráfico de Ie x frequência média de disparos é plotar com o respectivo
+  Erro Padrão da Média.
+  '''
 
   t = []
-  for i in range(0,100):
-
-    for g in range(0,120,5): #até 120nA com rampas de 5 em 5 
-        Ie = g
-        for step in range(len(tList)):
-          Is = 0
-          if np.random.rand() < freq*dt/1000:
-            Is = Ie
-          vList[step] = V
-          V = V + dt/tau*(El-V+Rm*Is)
-          if V > vThresh:
-            vList[step] = 0
-            V = vReset
-            freqz +=1
-        t = np.append(t,freqz)
+  for i in range(0,50):
+    
+    for g in range(1,251,10): #até 120nA com rampas de 5 em 5 
+      Ie = g
+      for step in range(len(tList)):
+        Is = 0
+        if np.random.rand() < freq*dt/1000:
+          Is = Ie
+        vList[step] = V
+        V = V + dt/tau*((El-V)+Rm*Is)
+        if V > vThresh:
+          vList[step] = 0
+          V = vReset
+          conta_freq +=1
+      t = np.append(t,conta_freq)
+      conta_freq = 0
   
-  #print(t[:20])
-  #print(t[300:320])       
-  #print(t[-20:])
-  #t = t.reshape(100,15)
-  #print(t[0,:])  
- 
+
+  t = t.reshape(50,25)
   md_freq = t.mean(axis=0)
-  std_freq =t.std(axis=0) 
+  std_freq = t.std(axis=0)
+  print(md_freq) 
+  print(std_freq)
+
+  #plotar os disparos para cada uma das correntes
   #plt.plot(tList, vList);
   #plt.savefig('plot.png');
  
-  #adicionar os std em yerr
-  plt.errorbar(len(range(0,120,5)),md_freq,yerr=std_freq);
+  plt.errorbar(range(1,251,10),md_freq,yerr=std_freq);
   plt.show()
-
-
-  #falta fazer a média e o std da matriz
 
 # Script ==================================
 integra()
